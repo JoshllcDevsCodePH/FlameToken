e981fbfbbd52c426d53b0396329655e0854d148a
JoshllcDevsCode <121450267+JoshllcDevsCodePH@users.noreply.github.com>
2024-06-24 16:34:07 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -180,7 +180,7 @@ const App = () => {
               </div>
             </div>
             <div className="flex-grow flex items-center max-w-60 text-sm">
-              <div className="w-full bg-[#eec203] py-4 rounded-2xl flex justify-around">
+              <div className="w-full bg-[#eec203] py-2 rounded-2xl flex justify-around">
                 <button className="flex flex-col items-center gap-1">
                   <img src={bear} width={24} height={24} alt="Bear" />
                   <span>Frens</span>
